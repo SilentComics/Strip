bade32ccc10ecbe457768e715a5024043f7a8cb5
SilentComics <hoa@silent-comics.com>
2015-04-28 17:26:24 +0200

    Update to WIP version 2.2.5.9

--- silentcomics/footer.php
+++ silentcomics/footer.php
@@ -10,10 +10,32 @@
 
 	</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="entry-wrap">
-			<?php get_sidebar( 'secondary' ); ?>
+<div class="entry-wrap wrap clear">
+			<?php if ( is_active_sidebar( 'footer-sidebar-1' ) || is_active_sidebar( 'footer-sidebar-2' ) || is_active_sidebar( 'footer-sidebar-3' ) ) : ?>
+	<div id="tertiary" class="widget-area clear" role="complementary">
+		<div class="widget-area-wrapper">
+			<?php do_action( 'before_sidebar' ); ?>
+			<?php if ( is_active_sidebar( 'footer-sidebar-1' ) ) : ?>
+				<div class="sidebar-1">
+					<?php dynamic_sidebar( 'footer-sidebar-1' ); ?>
+				</div>
+			<?php endif; ?>
+			<?php if ( is_active_sidebar( 'footer-sidebar-2' ) ) : ?>
+				<div class="sidebar-2">
+					<?php dynamic_sidebar( 'footer-sidebar-2' ); ?>
+				</div>
+			<?php endif; ?>
+			<?php if ( is_active_sidebar( 'footer-sidebar-3' ) ) : ?>
+				<div class="sidebar-3">
+					<?php dynamic_sidebar( 'footer-sidebar-3' ); ?>
+				</div>
+			<?php endif; ?>
+		</div><!-- .widget-area-wrapper -->
+	</div><!-- #secondary -->
+<?php endif; //is_active_sidebar ?>
 		</div>
+		
+	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
 			&copy; <?php 
   $fromYear = 2013; 
--- silentcomics/functions.php
+++ silentcomics/functions.php
@@ -98,28 +98,38 @@ add_action( 'after_setup_theme', 'silentcomics_setup' );
  */
 function silentcomics_widgets_init() {
 	register_sidebar( array(
+		'name'          => __( 'Primary Sidebar', 'silentcomics' ),
+		'id'            => 'sidebar-1',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h1 class="widget-title">',
+		'after_title'   => '</h1>',
+	) );
+
+	
+	register_sidebar( array(
 		'name'          => __( 'Footer Sidebar 1', 'silentcomics' ),
 		'id'            => 'footer-sidebar-1',
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</aside>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
+		'before_title'  => '<h3 class="widget-title">',
+		'after_title'   => '</h3>',
 	) );
 	register_sidebar( array(
 		'name'          => __( 'Footer Sidebar 2', 'silentcomics' ),
 		'id'            => 'footer-sidebar-2',
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</aside>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
+		'before_title'  => '<h3 class="widget-title">',
+		'after_title'   => '</h3>',
 	) );
 	register_sidebar( array(
 		'name'          => __( 'Footer Sidebar 3', 'silentcomics' ),
 		'id'            => 'footer-sidebar-3',
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</aside>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
+		'before_title'  => '<h3 class="widget-title">',
+		'after_title'   => '</h3>',
 		));
 }
 add_action( 'widgets_init', 'silentcomics_widgets_init' );
