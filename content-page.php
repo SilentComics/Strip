647830d68c94c4d0ea364d124f984ea1ed91df59
SilentComics <hoa@silent-comics.com>
2016-09-19 20:46:01 +0200

    reformat

--- content-page.php
+++ content-page.php
@@ -7,29 +7,29 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<div class="entry-wrap wrap clear">
-		<?php
-			if ( '' != get_the_post_thumbnail() ) :
-				the_post_thumbnail( 'silentcomics-featured-thumbnail' );
-			endif;
-		?>
-		<header class="entry-header">
-			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-		</header><!-- .entry-header -->
+    <div class="entry-wrap wrap clear">
+    <?php
+    if ('' != get_the_post_thumbnail()) :
+        the_post_thumbnail('silentcomics-featured-thumbnail');
+    endif;
+    ?>
+        <header class="entry-header">
+    <?php the_title('<h1 class="entry-title">', '</h1>'); ?>
+        </header><!-- .entry-header -->
 
-	<footer class="entry-meta">
-			<?php silentcomics_entry_meta(); ?>
+    <footer class="entry-meta">
+    <?php silentcomics_entry_meta(); ?>
 
-			<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
-			<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'silentcomics' ), __( '1 Comment', 'silentcomics' ), __( '% Comments', 'silentcomics' ) ); ?></span>
-			<?php endif; ?>
+    <?php if (! post_password_required() && ( comments_open() || '0' != get_comments_number() )) : ?>
+            <span class="comments-link"><?php comments_popup_link(__('Leave a comment', 'silentcomics'), __('1 Comment', 'silentcomics'), __('% Comments', 'silentcomics')); ?></span>
+    <?php endif; ?>
 
-			<?php edit_post_link( __( 'Edit', 'silentcomics' ), '<span class="edit-link">', '</span>' ); ?>
-		</footer><!-- .entry-meta -->
+    <?php edit_post_link(__('Edit', 'silentcomics'), '<span class="edit-link">', '</span>'); ?>
+        </footer><!-- .entry-meta -->
 
-		<div class="entry-content clear">
-			<?php the_content(); ?>
+        <div class="entry-content clear">
+    <?php the_content(); ?>
 
-		</div><!-- .entry-content -->
-	</div><!-- .entry-wrap -->
+        </div><!-- .entry-content -->
+    </div><!-- .entry-wrap -->
 </article><!-- #post-## -->
