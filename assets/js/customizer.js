19db856d7939c0a343bf522bb9419bd699923f6b
SilentComics <hoa@silent-comics.com>
2017-01-26 01:19:51 +0100

    minor fixes, remove unused code

--- assets/js/customizer.js
+++ assets/js/customizer.js
@@ -1,36 +1,23 @@
 /**
- * Live-update changed settings in real time in the Customizer preview.
+ * Theme Customizer enhancements for a better user experience.
  *
  * Contains handlers to make Theme Customizer preview reload changes asynchronously.
  */
 
-/*jshint latedef: nofunc */
-/** global: wp */
 ( function( $ ) {
-	var style = $( '#strip-color-scheme-css' ),
-		api = wp.customize;
-
-	if ( ! style.length ) {
-		style = $( 'head' ).append( '<style type="text/css" id="strip-color-scheme-css" />' )
-		                    .find( '#strip-color-scheme-css' );
-	}
-
-	// Site title.
-	api( 'blogname', function( value ) {
+	// Site title and description.
+	wp.customize( 'blogname', function( value ) {
 		value.bind( function( to ) {
-			$( '.site-title a' ).text( to );
+			$( '.site-title' ).text( to );
 		} );
 	} );
-
-	// Site tagline.
-	api( 'blogdescription', function( value ) {
+	wp.customize( 'blogdescription', function( value ) {
 		value.bind( function( to ) {
 			$( '.site-description' ).text( to );
 		} );
 	} );
-
-	// Header text color
-		api( 'header_textcolor', function( value ) {
+	// Header text color.
+	wp.customize( 'header_textcolor', function( value ) {
 		value.bind( function( to ) {
 			if ( 'blank' === to ) {
 				$( '.site-title, .site-description' ).css( {
@@ -40,24 +27,16 @@
 			} else {
 				$( '.site-title, .site-description' ).css( {
 					'clip': 'auto',
-					'color': to,
-					'position': 'relative'
+					'position': 'static'
+				} );
+
+				$( '.site-title a, .site-description' ).css( {
+					'color': to
+				} );
+				$( '.site-title a' ).css( {
+					'border-color': to
 				} );
 			}
-		} );
-	} );
-
-	// Add custom-background-image body class when background image is added.
-	api( 'background_image', function( value ) {
-		value.bind( function( to ) {
-			$( 'body' ).toggleClass( 'custom-background-image', '' !== to );
-		} );
-	} );
-
-	// Color Scheme CSS.
-	api.bind( 'preview-ready', function() {
-		api.preview.bind( 'update-color-scheme-css', function( css ) {
-			style.html( css );
 		} );
 	} );
 } )( jQuery );
