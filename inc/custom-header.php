80236c8154948bdc5af95e6d60dd87738d051743
SilentComics <hoa@silent-comics.com>
2017-01-30 01:54:11 +0100

    correct errors that prevented customising site title & description colour

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -27,16 +27,16 @@ function strip_custom_header_setup() {
 		'flex-height'            => true,
 		'wp-head-callback'       => 'strip_header_style',
 		'admin-head-callback'    => 'strip_admin_header_style',
-		'admin-preview-callback' => 'strip_admin_header_image',
+			'admin-preview-callback' => 'strip_admin_header_image',
 	) ) );
 }
-add_action( 'after_setup_theme', 'strip_custom_header_setup' );
+	add_action( 'after_setup_theme', 'strip_custom_header_setup' );
 
 if ( ! function_exists( 'strip_header_style' ) ) :
 
 	/**
-* Register default header image
-*/
+ * Register default header image
+ */
 	register_default_headers( array(
 		'DefaultHeader' => array(
 		'url'  			=> get_template_directory_uri() . '/assets/images/Default-header.png',
@@ -50,75 +50,75 @@ if ( ! function_exists( 'strip_header_style' ) ) :
 	 * @see strip_custom_header_setup().
 	 */
 	function strip_header_style() {
-		$header_text_color = get_header_textcolor();
+		  $header_text_color = get_header_textcolor();
 
-		// If no custom options for text are set, let's bail
-		// get_header_textcolor() options: add_theme_support( 'custom-header' ) is default, hide text (returns 'blank') or any hex value.
+		  // If no custom options for text are set, let's bail
+		  // get_header_textcolor() options: add_theme_support( 'custom-header' ) is default, hide text (returns 'blank') or any hex value.
 		if ( get_theme_support( 'custom-header', 'default-text-color' ) === $header_text_color ) {
 			return;
 		}
-		// If we get this far, we have custom styles. Let's do this.
+		  // If we get this far, we have custom styles. Let's do this.
+			?>
+		  <style type="text/css">
+			<?php
+			// Has the text been hidden?
+			if ( 'blank' === $header_text_color ) :
 		?>
-		<style type="text/css">
+			.site-title,
+			.site-description {
+				position: absolute;
+				clip: rect(1px, 1px, 1px, 1px);
+			}
 		<?php
-		// Has the text been hidden?
-		if ( 'blank' === $header_text_color ) :
-	?>
-		.site-title,
-		.site-description {
-			position: absolute;
-			clip: rect(1px, 1px, 1px, 1px);
-		}
-	<?php
-		// If the user has set a custom color for the text use that.
-		return;
-	?>
-		.site-title a,
-		.site-description {
-			color: #<?php echo esc_html( $header_text_color ); ?>;
-		}
-		.site-title {
-			border-color: #<?php echo esc_html( $header_text_color ); ?>;
-		}
-	<?php endif; ?>
-
-	</style>
-	<?php
-	}
-endif; // strip_header_style.
-
-if ( ! function_exists( 'strip_admin_header_style' ) ) :
-	/**
-	 * Styles the header image displayed on the Appearance > Header admin panel.
-	 *
-	 * @see strip_custom_header_setup().
-	 */
-	function strip_admin_header_style() {
+			// If the user has set a custom color for the text use that.
+			else :
 		?>
-		<style type="text/css">
-		.appearance_page_custom-header #headimg {
-		border: none; }
-		#headimg h1,
-		#desc { }
+			.site-title a,
+			.site-description {
+				color: #<?php echo esc_attr( $header_text_color ); ?>;
+			}
+			.site-title {
+				border-color: #<?php echo esc_attr( $header_text_color ); ?>;
+			}
+		<?php endif; ?>
 
-		#headimg h1 {
-		font: 700 2.4rem/1.4166666666 Futura, "Trebuchet MS", Arial, sans-serif;
-		text-align: center;
-		text-transform: uppercase; }
-
-		#headimg h1 a {
-		border: 6px solid #424242;
-		font-size: 1.2em;
-		letter-spacing: 0.1em;
-		line-height: 1.24;
-		padding: 0 .76em; }
-
-		#desc { }
-		#headimg img { }
 		</style>
 		<?php
 	}
-endif; // strip_admin_header_style.
+	 endif; // strip_header_style.
+
+if ( ! function_exists( 'strip_admin_header_style' ) ) :
+	 /**
+	  * Styles the header image displayed on the Appearance > Header admin panel.
+	  *
+	  * @see strip_custom_header_setup().
+	  */
+	function strip_admin_header_style() {
+		?>
+	   <style type="text/css">
+	   .appearance_page_custom-header #headimg {
+			border: none; }
+	   #headimg h1,
+			#desc { }
+
+	   #headimg h1 {
+			font: 700 2.4rem/1.4166666666 Futura, "Trebuchet MS", Arial, sans-serif;
+			text-align: center;
+			text-transform: uppercase; }
+
+	   #headimg h1 a {
+			border: 6px solid #424242;
+			font-size: 1.2em;
+			letter-spacing: 0.1em;
+			line-height: 1.24;
+			padding: 0 .76em; }
+
+	   #desc { }
+	   #headimg img { }
+	   </style>
+		<?php
+	}
+	endif; // strip_admin_header_style.
 
 if ( ! function_exists( 'strip_admin_header_image' ) ) :
 	/**
@@ -127,15 +127,15 @@ if ( ! function_exists( 'strip_admin_header_image' ) ) :
 	 * @see strip_custom_header_setup().
 	 */
 	function strip_admin_header_image() {
-		$style        = sprintf( ' style="color:#%s;"', get_header_textcolor() );
+		  $style        = sprintf( ' style="color:#%s;"', get_header_textcolor() );
 		?>
-		<div id="headimg">
-		<h1 class="displaying-header-text"><a id="name"<?php echo esc_attr( $style ); ?> onclick="return false;" href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
-		<div class="displaying-header-text" id="desc"<?php echo esc_attr( $style ); ?>><?php bloginfo( 'description' ); ?></div>
-		<?php if ( get_header_image() ) : ?>
-		<img src="<?php header_image(); ?>" alt="">
-		<?php endif; ?>
-	</div><!-- #headimg -->
+	   <div id="headimg">
+			<h1 class="displaying-header-text"><a id="name"<?php echo $style; ?> onclick="return false;" href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
+			<div class="displaying-header-text" id="desc"<?php echo $style; ?>><?php bloginfo( 'description' ); ?></div>
+			<?php if ( get_header_image() ) : ?>
+			<img src="<?php header_image(); ?>" alt="">
+			<?php endif; ?>
+		</div><!-- #headimg -->
 	<?php
 	}
-endif; // strip_admin_header_image.
+	endif; // strip_admin_header_image.
