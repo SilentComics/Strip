9fbc712227689b9d9b2217a0ee4b05cfc579027c
SilentComics <hoa@silent-comics.com>
2017-01-30 14:39:36 +0100

    remove ()

--- assets/js/customizer.js
+++ assets/js/customizer.js
@@ -4,39 +4,39 @@
  * Contains handlers to make Theme Customizer preview reload changes asynchronously.
  */
 
-((function($, wp) {
-    // Site title and description.
-    wp.customize("blogname", function(value) {
-        value.bind(function(to) {
-            $(".site-title").text(to);
-        });
-    });
-    wp.customize("blogdescription", function(value) {
-        value.bind(function(to) {
-            $(".site-description").text(to);
-        });
-    });
-    // Header text color.
-    wp.customize("header_textcolor", function(value) {
-        value.bind(function(to) {
-            if ('blank' === to) {
-                $(".site-title, .site-description").css({
-                    "clip": "rect(1px, 1px, 1px, 1px)",
-                    "position": "absolute"
-                });
-            } else {
-                $(".site-title, .site-description").css({
-                    "clip": "auto",
-                    "color": to,
-                    "position": "static"
-                });
-                $(".site-title a, .site-description").css({
-                    "color": to
-                });
-                $(".site-title a").css({
-                    "border-color": to
-                });
-            }
-        });
-    });
-}))(jQuery);
+(function($, wp) {
+     // Site title and description.
+     wp.customize("blogname", function(value) {
+         value.bind(function(to) {
+             $(".site-title").text(to);
+         });
+     });
+     wp.customize("blogdescription", function(value) {
+         value.bind(function(to) {
+             $(".site-description").text(to);
+         });
+     });
+     // Header text color.
+     wp.customize("header_textcolor", function(value) {
+         value.bind(function(to) {
+             if ('blank' === to) {
+                 $(".site-title, .site-description").css({
+                     "clip": 'rect(1px, 1px, 1px, 1px)',
+                     "position": "absolute"
+                 });
+             } else {
+                 $(".site-title, .site-description").css({
+                     "clip": "auto",
+                     "color": to,
+                     "position": "static"
+                 });
+                 $('.site-title a, .site-description').css({
+                     "color": to
+                 });
+                 $(".site-title a").css({
+                     "border-color": to
+                 });
+             }
+         });
+     });
+ })(jQuery);
