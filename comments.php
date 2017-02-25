85b70142163e7345d3582fb7d49624f082b526ac
SilentComics <hoa@silent-comics.com>
2017-02-25 12:18:00 +0100

    escaping

--- comments.php
+++ comments.php
@@ -36,13 +36,15 @@ if ( post_password_required() ) {
 				printf( esc_html_x( 'One Reply to &ldquo;%s&rdquo;', 'comments title', 'strip' ), get_the_title() );
 			} else {
 				printf(
-					/* translators: 1: number of comments, 2: post title */
-					_nx(
-						'%1$s Reply to &ldquo;%2$s&rdquo;',
-						'%1$s Replies to &ldquo;%2$s&rdquo;',
-						$comments_number,
-						'comments title',
-						'strip'
+					esc_attr(
+						/* translators: 1: number of comments, 2: post title */
+						_nx(
+							'%1$s Reply to &ldquo;%2$s&rdquo;',
+							'%1$s Replies to &ldquo;%2$s&rdquo;',
+							$comments_number,
+							'comments title',
+							'strip'
+						)
 					),
 					number_format_i18n( $comments_number ),
 					get_the_title()
