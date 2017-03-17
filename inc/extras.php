f8bba67bc81d23c73a944140dde9fd7a38628723
SilentComics <hoa@silent-comics.com>
2017-03-17 21:33:20 +0100

    removed unused code in extras.php

--- inc/extras.php
+++ inc/extras.php
@@ -6,50 +6,40 @@
  *
  * @package WordPress
  * @subpackage Strip
+ * @since 1.1.2
  */
 
 /**
  * Adds custom classes to the array of body classes.
  *
- * @param $strings $classes group-blog.
+ * @param array $classes Classes for the body element.
+ * @return array
  */
 function strip_body_classes( $classes ) {
 	// Adds a class of group-blog to blogs with more than 1 published author.
 	if ( is_multi_author() ) {
 		$classes[] = 'group-blog';
 	}
-
+	// Add class of hfeed to non-singular pages.
+	if ( ! is_singular() ) {
+		$classes[] = 'hfeed';
+	}
+	// Add class if we're viewing the Customizer for easier styling of theme options.
+	if ( is_customize_preview() ) {
+		$classes[] = 'strip-customizer';
+	}
+	// Add class on front page.
+	if ( is_front_page() && 'posts' !== get_option( 'show_on_front' ) ) {
+		$classes[] = 'strip-front-page';
+	}
+	// Add a class if there is a custom header.
+	if ( has_header_image() ) {
+		$classes[] = 'has-header-image';
+	}
+	// Add class if the site title and tagline is hidden.
+	if ( 'blank' === get_header_textcolor() ) {
+		$classes[] = 'title-tagline-hidden';
+	}
 	return $classes;
 }
 add_filter( 'body_class', 'strip_body_classes' );
-
-/**
- * Filters wp_title to print a neat <title> tag based on what is being viewed.
- *
- * @param $strings $title $paged.
- * @param $strings $sep title.
- */
-function strip_wp_title( $title, $sep ) {
-	global $page, $paged;
-
-	if ( is_feed() ) {
-		return $title;
-	}
-
-	// Add the blog name.
-	$title .= get_bloginfo( 'name' );
-
-	// Add the blog description for the home/front page.
-	$site_description = get_bloginfo( 'description', 'display' );
-	if ( $site_description && ( is_home() || is_front_page() ) ) {
-		$title .= " $sep $site_description";
-	}
-
-	// Add a page number if necessary.
-	if ( $paged >= 2 || $page >= 2 ) {
-		$title .= " $sep " . sprintf( __( 'Page %s', 'strip' ), max( $paged, $page ) );
-	}
-
-	return $title;
-}
-add_filter( 'wp_title', 'strip_wp_title', 10, 2 );
