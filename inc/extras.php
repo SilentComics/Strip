c7bf140dafcc3d747566e600e4feeb06772ccd13
SilentComics <hoa@silent-comics.com>
2017-03-19 00:23:50 +0100

    remove the DNS prefetch by returning false on filter

--- inc/extras.php
+++ inc/extras.php
@@ -45,18 +45,12 @@ function disable_emojis() {
  * @return array Difference betwen the two arrays.
  */
 function disable_emojis_tinymce( $plugins ) {
-	if ( is_array( $plugins ) ) { return array_diff( $plugins, array( 'wpemoji' ) );
-	} else { return array();
-	} }
+	if ( is_array( $plugins ) ) {
+		return array_diff( $plugins, array( 'wpemoji' ) );
+	} return array();
+}
 	/**
 	 * Remove emoji CDN hostname from DNS prefetching hints.
-	 *
-	 * @param array  $urls URLs to print for resource hints.
-	 * @param string $relation_type The relation type the URLs are printed for.
-	 * @return array Difference betwen the two arrays.
 	 */
-function disable_emojis_remove_dns_prefetch( $urls, $relation_type ) {
-	if ( 'dns-prefetch' !== $relation_type ) { /** This filter is documented in wp-includes/formatting.php */
-		$emoji_svg_url = apply_filters( 'emoji_svg_url', 'https://s.w.org/images/core/emoji/2/svg/' );
-		$urls = array_diff( $urls, array( $emoji_svg_url ) );
-	}  return $urls; }
+
+add_filter( 'emoji_svg_url', '__return_false' );
