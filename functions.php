6d9aa032a13b060e8ef5538a3a9ce669b1452c6f
SilentComics <hoa@silent-comics.com>
2017-05-05 22:22:52 +0200

    filter_ptags_on_images

--- functions.php
+++ functions.php
@@ -78,6 +78,7 @@ if ( ! function_exists( 'strip_setup' ) ) :
 		 * @see http://codex.wordpress.org/Function_Reference/wpautop gets the same result but removes line blocks: remove_filter( 'the_content', 'wpautop' );
 		 */
 		function filter_ptags_on_images( $content ) {
+			$content = preg_replace( '/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(<\/a>)?\s*<\/p>/iU', '\1\2\3', $content );
 			return preg_replace( '/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(<\/a>)?\s*<\/p>/iU', '\1\2\3', $content );
 		}
 		add_filter( 'the_content', 'filter_ptags_on_images' );
