daff7c0199c49b41a82cf79469a3bd2cb552240f
Hoa <hoa@silent-comics.com>
2018-10-03 10:18:51 +0200

    minor improvement to customizer code

--- assets/js/customizer.js
+++ assets/js/customizer.js
@@ -4,20 +4,23 @@
  * Contains handlers to make Theme Customizer preview reload changes asynchronously.
  */
 
-(function($, wp) {
-     // Site title and description.
-     wp.customize("blogname", function(value) {
-         value.bind(function(to) {
-             $(".site-title").text(to);
-         });
-     });
-     wp.customize("blogdescription", function(value) {
-         value.bind(function(to) {
-             $(".site-description").text(to);
-         });
-     });
+( function( $, wp ) {
+    var api = wp.customize;
+
+    // Site title and description.
+    api( 'blogname', function( value ) {
+        value.bind( function( to ) {
+            $( '#header h1 a, #footer a.site-name' ).html( to );
+        } );
+    } );
+
+    api( 'blogdescription', function( value ) {
+        value.bind( function( to ) {
+            $( '#header p.site-description' ).html( to );
+        } );
+    } );
      // Header text color.
-     wp.customize("header_textcolor", function(value) {
+     api("header_textcolor", function(value) {
          value.bind(function(to) {
              if ("blank" === to) {
                  $(".site-title, .site-description").css({
