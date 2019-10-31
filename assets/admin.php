1cbafb4f916198ab7fa63b5a9e62ad8f4359a448
Hoa <hoa@silent-comics.com>
2019-10-31 17:39:15 +0100

    Update theme to version 1.2.3

--- assets/admin.php
+++ assets/admin.php
@@ -9,10 +9,6 @@
  * Adapted from Bones, developed by Eddie Machado.
  * URL: http://themble.com/bones/
  *
- * Special Thanks for code & inspiration to:
- *
- * @jackmcconnell - http://www.voltronik.co.uk/
- * Digging into WP - http://digwp.com/2010/10/customize-wordpress-dashboard/
  *
  * @package WordPress
  * @subpackage Strip
@@ -58,36 +54,76 @@ http://digwp.com/2010/10/customize-wordpress-dashboard/
 */
 
 /**
- * RSS Dasboars Widget
+ * RSS Dasboard Widget
  */
 function strip_rss_dashboard_widget() {
-	if ( function_exists( 'fetch_feed' ) ) {
-		$feed = fetch_feed( 'https://silentcomics.com/feed.xml' );// specify the source feed.
-		if ( is_wp_error( $feed ) ) {
-			return;
+	// Get RSS Feed(s)
+	include_once(ABSPATH . WPINC . '/feed.php');
+
+	// My feeds list (add your own RSS feeds urls)
+	$my_feeds = array(
+				'https://silentcomics.com/feed.xml'
+				);
+
+	// Loop through Feeds
+	foreach ( $my_feeds as $feed) :
+
+		// Get a SimplePie feed object from the specified feed source.
+		$rss = fetch_feed( $feed );
+		if (!is_wp_error( $rss ) ) : // Checks that the object is created correctly
+		    // Figure out how many total items there are, and choose a limit
+		    $maxitems = $rss->get_item_quantity( 3 );
+
+		    // Build an array of all the items, starting with element 0 (first element).
+		    $rss_items = $rss->get_items( 0, $maxitems );
+
+		    // Get RSS title
+		    $rss_title = '<a href="'.$rss->get_permalink().'" target="_blank">'.strtoupper( $rss->get_title() ).'</a>';
+		endif;
+
+		// Display the container
+		echo '<div class="rss-widget">';
+		echo '<strong>'.$rss_title.'</strong>';
+		echo '<hr style="border: 0; background-color: #DFDFDF; height: 1px;">';
+
+		// Starts items listing within <ul> tag
+		echo '<ul>';
+
+		// Check items
+		if ( $maxitems == 0 ) {
+			echo '<li>'.__( 'No item', 'rc_mdm').'.</li>';
+		} else {
+			// Loop through each feed item and display each item as a hyperlink.
+			foreach ( $rss_items as $item ) :
+				// Uncomment line below to display non human date
+				//$item_date = $item->get_date( get_option('date_format').' @ '.get_option('time_format') );
+
+				// Get human date (comment if you want to use non human date)
+				$item_date = human_time_diff( $item->get_date('U'), current_time('timestamp')).' '.__( 'ago', 'rc_mdm' );
+
+				// Start displaying item content within a <li> tag
+				echo '<li>';
+				// create item link
+				echo '<a href="'.esc_url( $item->get_permalink() ).'" title="'.$item_date.'">';
+				// Get item title
+				echo esc_html( $item->get_title() );
+				echo '</a>';
+				// Display date
+				echo ' <span class="rss-date">'.$item_date.'</span><br />';
+				// Get item content
+				$content = $item->get_content();
+				// Shorten content
+				$content = wp_html_excerpt($content, 120) . ' [...]';
+				// Display content
+				echo $content;
+				// End <li> tag
+				echo '</li>';
+			endforeach;
 		}
-			$limit = $feed->get_item_quantity( 3 );                        // specify number of items.
-			$items = $feed->get_items( 0, $limit );                        // create an array of items.
-	}
-	if ( 0 ) { echo '<div>The RSS Feed is either empty or unavailable.</div>'; // fallback message.
-		return;
-	}
-	foreach ( $items as $item ) { ?>
+		// End <ul> tag
+		echo '</ul></div>';
 
-			<h4 style="margin-bottom: 1em; max-width: 384px;">
-				<a href="<?php echo esc_url( $item->get_permalink() ); ?>" title="" target="_blank">
-					<?php echo esc_attr( $item->get_title() ); ?>
-					<br>
-				</a>
-			</h4>
-
-			<p style="margin-top: 1.2em;
-					  #strip_rss_dashboard_widget .postbox.postbox-container { width: 12px; }">
-						<?php echo esc_html( mysql2date( __( 'j F Y @ g:i a', 'strip' ), $item->get_date( 'Y-m-d H:i:s' ) ) ); ?>
-			<?php echo wp_kses_post( substr( $item->get_description(), 0, 742 ) ); ?>
-			</p>
-			<?php
-	}
+	endforeach; // End foreach feed
 }
 
 /**
